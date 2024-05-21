95b828bed828272dc1470c6ab7726093f1120236
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-21 23:37:04 +0530

    hide and show

---
+++ js/hideimg.js
@@ -0,0 +1,8 @@
+
+const myButton = document.getElementById("myButton");
+const myImg = document.getElementById("myImg");
+
+myButton.addEventListener("click", event => {
+    myImg.style.display = "none";
+    myButton.textContent = "show";
+})
